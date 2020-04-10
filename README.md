c0a40ae61f45c1b1b6a58070a3a95bb41b5412ae
pingyuan2016 <ziyi654321@gmail.com>
2020-04-10 08:10:17 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# file_server
-flask文件上传下载服务
+# Flask file server
+用flask实现文件上传下载服务
+
+###上传服务
+启动python app.py
+
+访问地址http://127.0.0.1:8067/
+
+
+### 下载服务（后面跟文件名称）
+#### 方法一
+访问地址http://127.0.0.1:8067/download/1.png
+
+#### 方法二 会用http.server
+到需要下载的目录
+
+python -m http.server
--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from werkzeug.utils import secure_filename
-from flask import Flask,render_template,jsonify,request
+from flask import Flask,render_template,abort,request
 from flask import send_from_directory
-import time
 import os
-import base64
+
 
 app = Flask(__name__)
 UPLOAD_FOLDER='upload'
@@ -17,36 +16,34 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
 
 # 用于测试上传，稍后用到
-@app.route('/test/upload')
+@app.route('/')
 def upload_test():
-    return render_template('upload.html')
+    return render_template('base.html')
 
 # 上传文件
-@app.route('/api/upload',methods=['POST'],strict_slashes=False)
+@app.route('/upload',methods=['GET','POST'],strict_slashes=False)
 def api_upload():
-    file_dir=os.path.join(basedir,app.config['UPLOAD_FOLDER'])
-    if not os.path.exists(file_dir):
-        os.makedirs(file_dir)
-    f=request.files['myfile']  # 从表单的file字段获取文件，myfile为该表单的name值
-    if f and allowed_file(f.filename):  # 判断是否是允许上传的文件类型
-        fname=secure_filename(f.filename)
-        print fname
-        ext = fname.rsplit('.',1)[1]  # 获取文件后缀
-        unix_time = int(time.time())
-        new_filename=str(unix_time)+'.'+ext  # 修改了上传的文件名
-        f.save(os.path.join(file_dir,new_filename))  #保存文件到upload目录
-        token = base64.b64encode(new_filename)
-        print token
-
-        return jsonify({"errno":0,"errmsg":"上传成功","token":token})
+    if request.method == 'GET':
+        return render_template('base.html')
     else:
-        return jsonify({"errno":1001,"errmsg":"上传失败"})
-@app.route('/download/<filename>')
+        file_dir=os.path.join(basedir,app.config['UPLOAD_FOLDER'])
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+        f=request.files['file']  # 从表单的file字段获取文件，myfile为该表单的name值
+        if f and allowed_file(f.filename):  # 判断是否是允许上传的文件类型
+            file = f.filename
+            f.save(os.path.join(file_dir,file))  #保存文件到upload目录
+            return render_template('base.html')
+        else:
+            return render_template('base.html')
+            # return jsonify({"errno":1001,"errmsg":"上传失败"})
+
+@app.route('/download/<filename>', methods=['GET'])
 def download(filename):
-    if request.method=="GET":
-        if os.path.isfile(os.path.join('upload', filename)):
-            #return send_from_directory('upload',filename,as_attachment=True)
-            return send_from_directory('upload',filename, as_attachment=False)
+    if request.method =="GET":
+        file_path =os.path.join(basedir,app.config['UPLOAD_FOLDER'])
+        if os.path.join(file_path, filename):
+            return send_from_directory(file_path,filename, as_attachment=True)
         abort(404)
 
 if __name__ == '__main__':
